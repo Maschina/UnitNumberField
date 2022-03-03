8c529e3628616dc52fc285878c740cad38679368
Robert Hahn <robert.hahn@mailbox.org>
2022-03-03 14:47:44 +0100

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# UnitNumberField
+
+A description of this package.
---
+++ Tests/UnitNumberFieldTests/UnitNumberFieldTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import UnitNumberField
+
+final class UnitNumberFieldTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(UnitNumberField().text, "Hello, World!")
+    }
+}
